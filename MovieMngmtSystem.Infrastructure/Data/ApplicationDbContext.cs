b0a884176a6f7e3599c0e143d4172ff513f1272b
valthial <valthial@gmail.com>
2025-03-03 18:18:31 +0200

    update db with deleted fields

--- MovieMngmtSystem.Infrastructure/Data/ApplicationDbContext.cs
+++ MovieMngmtSystem.Infrastructure/Data/ApplicationDbContext.cs
@@ -22,6 +22,8 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
             entity.Property(m => m.ImageUrl).HasMaxLength(500);
             entity.Property(m => m.Rating).HasColumnType("decimal(3,1)");
             entity.Property(m => m.TrailerUrl).HasMaxLength(500);
+            entity.Property(m => m.IsDeleted).IsRequired().HasDefaultValue(false);
+            entity.Property(m => m.DeletedAt).IsRequired(false);
         });
     }
 }
